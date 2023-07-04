01ee75778019e80b9bb81d3f604b9a9a9993d943
edmozer <131734732+edmozer@users.noreply.github.com>
2023-07-04 14:30:03 -0600

    Add files via upload

---
+++ projetoGaia/src/test/java/executores/Executora.java
@@ -0,0 +1,10 @@
+package executores;
+
+import robos.GaiaRobo;
+
+public class Executora {
+    public static void main(String[] args) {
+        GaiaRobo umaGaiaRobo = new GaiaRobo();
+        umaGaiaRobo.abrirChrome();
+    }
+}
---
+++ projetoGaia/src/test/java/robos/GaiaRobo.java
@@ -0,0 +1,21 @@
+package robos;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.sql.SQLOutput;
+
+public class GaiaRobo {
+    public void abrirChrome(){
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver edDriver = new ChromeDriver();
+
+        edDriver.get("http://www.juliodelima.com.br/taskit");
+    }
+    public void fazerLogin(){
+        System.out.println("Vou fazer login!");
+    }
+    public void lancarNota() {
+        System.out.println("Vou lancar uma nota!");
+    }
+}
